86546995dd2b927c3af60ac0a5ade6b75f285ccf
Eva Doychinova <evadoychinova@gmail.com>
2021-03-27 19:56:01 +0200

    Initial setup firebase and reactstrap

---
+++ config/firebase.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBBpD4PmlwAzMm4mHtkukSxCeDNJKnZdYM",
+    authDomain: "petadopt-cc847.firebaseapp.com",
+    projectId: "petadopt-cc847",
+    storageBucket: "petadopt-cc847.appspot.com",
+    messagingSenderId: "623937183677",
+    appId: "1:623937183677:web:852ff9869eee4229fa3cd4",
+    measurementId: "G-LGYGYX75P7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
